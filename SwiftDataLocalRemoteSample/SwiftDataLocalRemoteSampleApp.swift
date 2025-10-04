c2ae3eaa5ce734b43e575c7b65bf54e218a94711
Thomas Leese <thomas@leese.io>
2025-10-04 20:58:47 +0100

    Create project

---
+++ SwiftDataLocalRemoteSample/SwiftDataLocalRemoteSampleApp.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftDataLocalRemoteSampleApp.swift
+//  SwiftDataLocalRemoteSample
+//
+//  Created by Thomas Leese on 04/10/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwiftDataLocalRemoteSampleApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
